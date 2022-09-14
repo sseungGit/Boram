30b933292a7e619032985709ae0cd64378e78a19
HwangYiran <yiran023h@gmail.com>
2022-09-14 16:47:57 +0900

    review controller

--- Boram/src/main/java/com/acorn/boram/review/controller/KjReviewController.java
+++ Boram/src/main/java/com/acorn/boram/review/controller/KjReviewController.java
@@ -2,7 +2,7 @@ package com.acorn.boram.review.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class KjReviewController {
 
 	@RequestMapping("/review/list")
