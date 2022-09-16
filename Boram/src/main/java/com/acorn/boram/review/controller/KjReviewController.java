f83a9382a88e31e09dcd2752e4d53db97ca1e651
Gwak Jun <kj9589@naver.com>
2022-09-16 12:44:58 +0900

    review mapper, 게시판 양식작성

--- Boram/src/main/java/com/acorn/boram/review/controller/KjReviewController.java
+++ Boram/src/main/java/com/acorn/boram/review/controller/KjReviewController.java
@@ -1,12 +1,21 @@
 package com.acorn.boram.review.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class KjReviewController {
 
 	@RequestMapping("/review/list")
-	public String qnaPage() {
+	public String reviewPage() {
 		return "review/list";
+	}
+	
+	@RequestMapping("/review/insertform")
+	public ModelAndView authInsertForm(HttpServletRequest request) {
+		
+		return new ModelAndView("review/insertform");
 	}
 }
